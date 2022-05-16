cbb11a8a8d8420f9e82cea5241d63d5dc0e82f08
Mateo Gomez <matunik8@gmail.com>
2022-05-16 17:17:25 -0300

    TP1

---
+++ TP1.h
@@ -0,0 +1,33 @@
+#ifndef TP1_H_INCLUDED
+#define TP1_H_INCLUDED
+
+/*
+//EJERCICIO 1
+float superficieCirculo(int radio)
+{
+
+    return(3.1416 * radio * radio);
+
+}
+
+*/
+//EJERCICIO 2
+long suma(int desde, int hasta)
+{
+    for(int desde = i; i == hasta; ++i)
+        tot= i + hasta
+    return (tot);
+}
+
+
+
+/*
+
+//EJERCICIO 9
+void tablaDel(int n, int *tabla)
+{
+    for(int i = 0; i < 10; ++i)
+        tabla[i]= n * (i + 1);
+}
+*/
+#endif // TP1_H_INCLUDED
--- main.cpp
+++ main.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
+#include "TP1.h"
 
 using namespace std;
 
+//EJERCICIO 2
 int main()
 {
-    cout << "Hello world!" << endl;
+    int desde, hasta;
+     cout<< "Ingrese valor de desde : " << endl;
+     cin >> desde;
+     cout<< "Ingrese valor de hasta : " << endl;
+     cin >> hasta;
+     cout << suma(desde, hasta) << endl;
     return 0;
 }
+
+
